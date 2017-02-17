57eeab9e1a045fb6eed85918164d28d0f6829a92
Eduglez94 <eduglezzvl_ing@outlook.es>
2017-02-16 23:51:57 -0600

    Add files via upload

--- js/PersonalPage.js
+++ js/PersonalPage.js
@@ -1,1 +1,38 @@
+$(document).ready(function(){
+	var altura = $('.Menu').offset().top;
 
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.Menu').addClass('MenuFixed');
+		} else {
+			$('.Menu').removeClass('MenuFixed');
+		}
+    
+	});
+});
+$(function(){
+
+     $('a[href*=#]').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top;
+
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                 return false;
+
+            }
+
+       }
+
+   });
+
+});
